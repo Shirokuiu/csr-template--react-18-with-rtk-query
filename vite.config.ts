d366ffab8a6759c177029805b1795c2b5d7cb655
pavel <shirokuiuna@mail.ru>
2023-11-01 16:04:05 +0300

    переход на vite

---
+++ vite.config.ts
@@ -0,0 +1,16 @@
+import { defineConfig } from 'vite';
+import svgr from 'vite-plugin-svgr';
+import viteTsconfigPaths from 'vite-tsconfig-paths';
+import react from '@vitejs/plugin-react';
+
+export default defineConfig({
+  // depending on your application, base can also be "/"
+  base: '',
+  plugins: [react(), viteTsconfigPaths(), svgr({ include: '**/*.svg?react' })],
+  server: {
+    // this ensures that the browser opens upon server start
+    open: true,
+    // this sets a default port to 3000
+    port: 3000,
+  },
+});
